60035d6974d50451661a6763dd82b49167b14522
Allan Pedin <31974198+AllanPedin@users.noreply.github.com>
2023-09-18 21:18:05 -0400

    working but rate limit

--- market_researcher.py
+++ market_researcher.py
@@ -22,23 +22,32 @@ def check_websites_for_terms(csv_file, search_terms):
         website = row['Website']
         
         for term in search_terms:
-            query = f"site:{website} {term}"
-            
-            # Perform the Google search
-            search_results = search(query, num_results=100)
-            
-            # Count the number of pages from the website that contain the term
-            count = sum(website in result['url'] for result in search_results)
-            
-            # Update the DataFrame
-            df.at[index, f'PagesWith_{term}'] = count
-            
-            print(f"Processed {website}. Found {count} pages containing '{term}'.")
+            try:
 
-            # Introduce a delay
-            time.sleep(randint(30,40))
-        # Save the DataFrame to a new CSV file (Continuously so progress isnt lossed due to rate limiting)
-        df.to_csv("output_with_page_counts.csv", index=False)   
+                query = f"site:{website} {term}"
+                
+                # Perform the Google search
+                search_results = search(query, num_results=100)
+                
+                # Count the number of pages from the website that contain the term
+                count = sum(website in result['url'] for result in search_results)
+                
+                # Update the DataFrame
+                df.at[index, f'PagesWith_{term}'] = count
+                
+                print(f"Processed {website}. Found {count} pages containing '{term}'.")
+
+                # Introduce a delay
+                time.sleep(randint(30,40))
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                
+                # Save the DataFrame to a new CSV file
+                df.to_csv("output_with_page_counts.csv", index=False)
+                
+                return
+    # Save the DataFrame to a new CSV file
+    df.to_csv("output_with_page_counts.csv", index=False)   
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check company websites for specific terms.')
