87804de427794863900e8d64240ec1ceffa27e7c
Allan Pedin <31974198+AllanPedin@users.noreply.github.com>
2023-09-18 21:06:22 -0400

    initial commit

---
+++ market_researcher.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import argparse
+from random import randint
+import time
+from googlesearch import search
+
+def check_websites_for_terms(csv_file, search_terms):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+    
+    # Ensure the DataFrame has a 'Website' column
+    if 'Website' not in df.columns:
+        print("The CSV file must contain a 'Website' column.")
+        return
+    
+    # Create new columns for each search term
+    for term in search_terms:
+        df[f'PagesWith_{term}'] = 0
+    
+    # Loop through each website and perform Google searches
+    for index, row in df.iterrows():
+        website = row['Website']
+        
+        for term in search_terms:
+            query = f"site:{website} {term}"
+            
+            # Perform the Google search
+            search_results = search(query, num_results=100)
+            
+            # Count the number of pages from the website that contain the term
+            count = sum(website in result['url'] for result in search_results)
+            
+            # Update the DataFrame
+            df.at[index, f'PagesWith_{term}'] = count
+            
+            print(f"Processed {website}. Found {count} pages containing '{term}'.")
+
+            # Introduce a delay
+            time.sleep(randint(30,40))
+        # Save the DataFrame to a new CSV file (Continuously so progress isnt lossed due to rate limiting)
+        df.to_csv("output_with_page_counts.csv", index=False)   
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Check company websites for specific terms.')
+    parser.add_argument('terms', type=str, nargs='+', help='The terms to search for.')
+    
+    args = parser.parse_args()
+    
+    csv_file = "company_websites.csv"  # Replace with the path to your CSV file
+    search_terms = args.terms  # The terms are now taken from the command-line arguments
+    
+    check_websites_for_terms(csv_file, search_terms)
